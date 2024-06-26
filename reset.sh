4f7796cd112d99419b84d177d76b7b7185a8e343
Fernando Garibay <96392505+FernandoGaribay@users.noreply.github.com>
2024-06-25 18:01:00 -0600

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Lista de paquetes fundamentales
+# Lista de paquetes a no borrar (fundamentales)
 essential_packages=(
     'base'
     'linux'
@@ -12,36 +12,20 @@ essential_packages=(
     'systemd'
     'systemd-sysvcompat'
     'pacman'
+    'hwdata' # Añadir cualquier otro paquete fundamental que cause problemas de dependencia
 )
 
-# Crear un archivo temporal para almacenar la lista de paquetes fundamentales
-essential_packages_file=$(mktemp)
-for package in "${essential_packages[@]}"; do
-    echo $package >> $essential_packages_file
-done
+# Marcar todos los paquetes instalados como dependencias
+sudo pacman -D --asdeps $(pacman -Qqe)
 
-# Lista de todos los paquetes instalados
-installed_packages=$(pacman -Qq)
+# Marcar la lista de paquetes esenciales como explícitos
+sudo pacman -D --asexplicit "${essential_packages[@]}"
 
-# Crear un archivo temporal para almacenar los paquetes a eliminar
-packages_to_remove_file=$(mktemp)
-
-# Filtrar los paquetes que no están en la lista de paquetes fundamentales
-for package in $installed_packages; do
-    if ! grep -qx $package $essential_packages_file; then
-        echo $package >> $packages_to_remove_file
-    fi
-done
-
-# Desinstalar los paquetes no fundamentales
-xargs -a $packages_to_remove_file sudo pacman -Rns --noconfirm
+# Borrar todos los paquetes que se hayan quedado como dependencias
+sudo pacman -Rsn $(pacman -Qttdq)
 
 # Limpiar la base de datos de paquetes
 sudo pacman -Scc --noconfirm
-
-# Eliminar archivos temporales
-rm $essential_packages_file
-rm $packages_to_remove_file
 
 # Preguntar al usuario si desea reiniciar el sistema
 read -p "¿Desea reiniciar el sistema ahora? (s/n): " answer
